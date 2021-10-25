a7fb67181b0cdfc1bc461d13e3977b7a3738dc9c
fkenned <fkenned@student.21-school.ru>
2021-10-26 01:05:24 +0300

    w

--- tyt/main.c
+++ tyt/main.c
@@ -1,59 +1,59 @@
 #include "minishell.h"
 
-// t_envp  *list_envp(t_envp *list, char *str_env)
-// {
-// 	t_envp	*new_list;
-// 	void	*save;
+t_envp  *list_envp(t_envp *list, char *str_env)
+{
+	t_envp	*new_list;
+	void	*save;
 
-// 	if (!list)
-// 	{
-// 		list = (t_envp *)malloc(sizeof(t_envp));
-// 		list->var = str_env;
-// 		list->next = NULL;
-// 	}
-// 	else
-// 	{
-// 		save = list;
-// 		new_list = (t_envp *)malloc(sizeof(t_envp));
-// 		while (list->next != NULL)
-// 			list = list->next;
-// 		list->next = new_list;
-// 		list = save;
-// 		new_list->var = str_env;
-// 		new_list->next = NULL;
-// 	}
-// 	return (list);
-// }
+	if (!list)
+	{
+		list = (t_envp *)malloc(sizeof(t_envp));
+		list->var = str_env;
+		list->next = NULL;
+	}
+	else
+	{
+		save = list;
+		new_list = (t_envp *)malloc(sizeof(t_envp));
+		while (list->next != NULL)
+			list = list->next;
+		list->next = new_list;
+		list = save;
+		new_list->var = str_env;
+		new_list->next = NULL;
+	}
+	return (list);
+}
 
-// void	envp_print(t_envp *list, int pointer)
-// {
-// 	t_envp *p;
+void	envp_print(t_envp *list, int pointer)
+{
+	t_envp *p;
 
-// 	p = list;
-// 	while (p != NULL)
-// 	{
-// 		if (pointer == 1)
-// 			printf("declare -x ");
-// 		printf("%s\n", p->var);
-// 		p = p->next;
-// 	}
-// }
+	p = list;
+	while (p != NULL)
+	{
+		if (pointer == 1)
+			printf("declare -x ");
+		printf("%s\n", p->var);
+		p = p->next;
+	}
+}
 
-// t_envp	*make_envp(t_envp *list, char **envp)
-// {
-// 	t_envp *p;
-// 	void	*save;
-// 	int i;
+t_envp	*make_envp(t_envp *list, char **envp)
+{
+	t_envp *p;
+	void	*save;
+	int i;
 
-// 	i = 0;
-// 	p = list;
-// 	while (envp[i])
-// 	{
-// 		p = list_envp(p, envp[i]);
-// 		i++;
-// 	}
-// 	return (p);
-// }
+	i = 0;
+	p = list;
+	while (envp[i])
+	{
+		p = list_envp(p, envp[i]);
+		i++;
+	}
+	return (p);
+}
 
 int main(int argc, char **argv, char **envp)
 {
@@ -61,12 +61,12 @@ int main(int argc, char **argv, char **envp)
     int i = 0;
 
 	// data.env = NULL;
-	// data.exp = NULL;
-	// // data.env = make_envp(data.env, envp);
-	// data.exp = make_envp(data.exp, envp);
+	data.exp = NULL;
+	// data.env = make_envp(data.env, envp);
+	data.exp = make_envp(data.exp, envp);
 	
-	// // ft_env(&data);
-	// ft_export(&data);
+	// ft_env(&data);
+	ft_export(&data);
 
     while (i < 1)
     {
@@ -75,6 +75,6 @@ int main(int argc, char **argv, char **envp)
             parser(&data);
         i++;
     }
-	// clean_export(data.exp);
+	clean_export(data.exp);
 	
 }
--- tyt/quotes_test.c
+++ tyt/quotes_test.c
@@ -1,38 +1,140 @@
 #	include "minishell.h"
 
-char	*check_dollar(t_data *data, char *str)
+int compare_dollar_and_exp(char *exp, char *dollar)
+{
+	int i;
+
+	i = 0;
+	while(exp[i] && exp[i] != '=')
+	{
+		if (exp[i] != dollar[i])
+			return(1);
+		i++;
+	}
+	if (dollar[i] != '\0')
+		return (1);
+	return (0);
+}
+
+char	*search_envp_dollar(t_data *data, char *dollar)
+{
+	t_envp *p;
+	int i;
+
+	i = 0;
+	p = data->exp;
+	while (p != NULL)
+	{
+		if (compare_dollar_and_exp(p->var, dollar) == 0)
+		{
+			// printf("ok\n");
+			while (p->var[i] && p->var[i] != '=')
+				i++;
+			free(dollar);
+			dollar = ft_substr(p->var, i + 2, ft_strlen(p->var) - (i + 3));
+			return (dollar);
+		}		
+		p = p->next;
+	}
+	free(dollar);
+	return (ft_strdup(""));
+}
+
+char	*check_dollar(t_data *data, char *str, int lenght_str, int cmd_len)
 {
 	int i = 0;
 	char *save1 = NULL;
 	char *save2 = NULL;
 	int len_str = ft_strlen(str);
+	char *new;
+	char *save_new = NULL;
+	char *dollar = NULL;
+	int start = 0;
 
+	new = NULL;
+	
+	// printf("LEN_STR: %d cmd_len: %d\n", lenght_str, cmd_len);
+	printf("str: |%s|\n", str);
 	// не работает если строка будет типо: $PATHlol (надо искать будет переменную в массивах экспорта и локальных) 
-	while (str[i])
+	while (i < ft_strlen(str))
 	{
-		if (str[i] == '$')
+		if (str[i] == '$' && str[i + 1] != '\0')
 		{
 			i++;
-			if (i - 1 != 0)
-				save1 = ft_substr(str, 0, i - 1);
-			while (str[i] && str[i] != 34 && str[i] != 39 && str[i] != '$')
-				i++;
-			printf("i: %d len_str: %d\n", i, len_str);
-			if (i != len_str)
-				save2 = ft_substr(str, i, len_str - i);
-			free(str);
-			str = ft_strjoin(save1, "ABOBA");
-			free(save1);
-			if (i != len_str)
+			start = i;
+			if (new == NULL)
 			{
-				str = ft_strjoin(str, save2);
+				save1 = ft_substr(str, 0, i - 1);
+				save_new = new;
+				new = ft_strjoin(save_new, save1);
+				free(save_new);
+				free(save1);
+			}	
+			while (str[i] && str[i] != '$' && str[i] != ' ' && str[i] != 39 && str[i] != '|' && str[i] > 47)
+				i++;
+			dollar = ft_substr(str, start, i - start);
+			// printf("dollar: %s\n", dollar);
+			dollar = search_envp_dollar(&(*data), dollar);
+			// printf("dollar: %s\n", dollar);
+			start = i;
+			while (str[i] && str[i] != '$')
+				i++;
+			if (i != start)
+				save2 = ft_substr(str, start, i - start);
+			save_new = new;
+			new = ft_strjoin(save_new, dollar);
+			free(dollar);
+			free(save_new);
+			if (i != start)
+			{
+				save_new = new;
+				new = ft_strjoin(save_new, save2);
+				free(save_new);
 				free(save2);
 			}
 			if (str[i] == '$')
 				i--;
-			// break;
+		}
+		else if (str[i] == '$' && str[i + 1] == '\0' && cmd_len + i == lenght_str - 1)
+		{
+			if (new == NULL)
+			{
+				save1 = ft_substr(str, 0, i + 1);
+				save_new = new;
+				new = ft_strjoin(save_new, save1);
+				free(save_new);
+				free(save1);
+			}
+			else
+			{
+				save_new = new;
+				new = ft_strjoin(save_new, "$");
+				free(save_new);
+			}
 		}
 		i++;
+	}
+	i = 0;
+	if (new != NULL)
+	{
+		free(str);
+		// printf("new: |%s|\n", new);
+		return(new);
+	}
+	else
+	{
+		while (str[i])
+		{
+			if (str[i] == '$' && str[i + 1] == '\0')
+			{
+				save_new = str;
+				str = ft_substr(save_new, 0, i);
+				// printf("STRRRRR: |%s|\n", str);
+				free(save_new);
+				break ;
+			}
+			i++;
+		}
 	}
 	return (str);
 }
@@ -43,6 +145,7 @@ int	it_quotes(t_data *data, char **new, char *str, int i)
 	int quot;
 	char *save1;
 	char *save3;
+	char *save2;
 
 	save1 = NULL;
 	save3 = NULL;
@@ -57,7 +160,7 @@ int	it_quotes(t_data *data, char **new, char *str, int i)
 	{
 		save1 = ft_substr(str, start + 1, i - (start + 1));
 		if (quot == 34)
-			save1 = check_dollar(&(*data), save1);
+			save1 = check_dollar(&(*data), save1, ft_strlen(str), start);
 		save3 = *new;
 		*new = ft_strjoin(save3, save1);
 		free(save3);
@@ -76,7 +179,7 @@ int	not_quotes(t_data *data, char **new, char *str, int i)
 	while (str[i] && str[i] != 34 && str[i] != 39)
 		i++;
 	save1 = ft_substr(str, start, i - start);
-	save1 = check_dollar(&(*data), save1);
+	save1 = check_dollar(&(*data), save1, ft_strlen(str), start);
 	save3 = *new;
 	*new = ft_strjoin(save3, save1);
 	free(save3);
@@ -128,6 +231,7 @@ char	*ft_quot(t_data *data, char *str)
             i = 0;
     }
 	new = help_quot(&(*data), str, new, i);
+	// printf("new: %s\n", new);
 	free(str);
 	return (new);
 }
